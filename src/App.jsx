5752b269c1ee14993f7584c364be647229522b63
Request <devkotaapil194@gmail.com>
2025-09-19 16:10:27 +0545

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,9 @@ function App(){
     <div className="flex h-screen w-full justify-center items-center">
       <div  className="border border-indigo-600 p-10 bg-indigo-400 rounded-lg shadow-lg shadow-blue-400">
         <h1 className='text-center text-2xl text-white'>Search GitHub Profile</h1>
-        <input type="text" onChange={(e)=> setSearch(e.target.value)} 
+        <input type="text" 
+        value={search}
+        onChange={(e)=> setSearch(e.target.value)} 
         className="border w-full mt-2 border-3 border-white text-center text-white " autoFocus/>
         <motion.button
         onClick={handleSubmit}
@@ -32,10 +34,19 @@ function App(){
         className='block px-3 py-2 bg-indigo-600 text-white hover:cursor-pointer font-semibold rounded-lg shadow-lg mx-auto mt-5'
         >Search</motion.button>
 
-        <div className='flex justfiy-space-between'>
-          <img src="" alt="" />
-          <p></p>
+        {res && 
+           <div className='grid grid-cols-2 mt-2 p-2'>
+          <img src={res.avatar_url} alt={res.login}  className='w-40 h-40 '/>
+          <div className='text-white font-serif'>
+          <h3 className='ml-5 underline'>Username:</h3>
+          <h3 className='ml-5'> {res.name}</h3>
+          <p className='ml-5 mt-5 underline'>Bio:</p>
+          <p className='ml-5'>{res.bio}</p>
+          </div>
+          
         </div>
+        }
+     
       </div>
     </div>
   )
